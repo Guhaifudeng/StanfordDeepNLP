b11fde468fcc4cceb00e841938e9c1ff7ffc146a
user <1161661054@qq.com>
2017-04-20 16:29:08 +0800

    function class

---
+++ lecture1/pynp/class_greeter.py
@@ -0,0 +1,14 @@
+class Greeter(object):
+    #Constructor
+    def  __init__(self, name):
+        self.name = name #create an instance variable
+    #instance method
+    def  greet(self, loud = False):
+        if loud:
+            print 'HELLO, %s' % self.name.upper()
+        else:
+            print 'Hello, %s' % self.name
+
+g = Greeter('Fred')
+g.greet()
+g.greet(loud = True)
