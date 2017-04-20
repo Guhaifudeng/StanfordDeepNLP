36427dafbdc442e671b075136ba2d640819cbc13
user <1161661054@qq.com>
2017-04-20 23:33:18 +0800

    引入.mat出错

---
+++ lecture1/pynp/save_a.m
@@ -0,0 +1,5 @@
+% octave save_a.m
+% save matric to octave_a.mat
+a = ones(4,4)
+
+save("octave_a.mat",'a')
---
+++ lecture1/pynp/scipy_matlab_files.py
@@ -0,0 +1,7 @@
+# octave save_a.m
+# save matric to octave_a.mat
+#
+import scipy.io as sio
+ma = sio.loadmat("octave_a.mat")
+print ma['a'] #wrong
+print "----------"
